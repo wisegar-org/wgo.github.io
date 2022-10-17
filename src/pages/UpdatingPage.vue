baf5208971a4128f60fa50d5993a6a89d7e7c7e3
Yariel Rodriguez <yariel.re@gmail.com>
2022-10-17 15:48:31 +0200

    Update quasar site code

---
+++ src/pages/UpdatingPage.vue
@@ -0,0 +1,45 @@
+<style>
+.update {
+  background-image: url("../assets/on-update.jpg");
+  background-repeat: no-repeat;
+  background-size: cover;
+  border-radius: 10px;
+}
+</style>
+<template>
+  <div class="q-pa-md q-gutter-md">
+    <div class="row justify-between update" :style="divStyle">
+      <div class="text-center items-center"></div>
+      <div class="q-pt-xl text-center items-center" :style="divInnerStyle">
+        <img
+          src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg"
+          style="width: 150px; height: 150px"
+        />
+        <div class="text-h3 text-white text-center">
+          <a
+            class="text-h3 text-white text-center"
+            href="https://github.com/wisegar-org"
+            >wisegar.org su github.com</a
+          >
+        </div>
+        <div class="text-h6 text-white text-center">site under update</div>
+      </div>
+      <div class="text-center items-center"></div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+export default defineComponent({
+  name: "UpdatingPage",
+  components: {},
+  setup() {
+    const screenH = screen.availHeight - screen.availHeight * 0.1;
+    const topPos = screenH - screenH / 3;
+    const divStyle = `height: ${screenH}px; top: ${topPos}`;
+    const divInnerStyle = `margin-top: ${topPos}`;
+    return { screenH, divStyle, divInnerStyle };
+  },
+});
+</script>
